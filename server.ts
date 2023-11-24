8cd88e3f59bc6feb6fc12f1c0d4a7ad992a2725e
nitroBW <24616266+nitrobw@users.noreply.github.com>
2023-11-24 13:41:20 +0100

    Initialize bun

---
+++ server.ts
@@ -0,0 +1,11 @@
+import { Database } from "bun:sqlite";
+import { Elysia } from "elysia";
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
+
+
+const db = new Database("shortened_links.db");
